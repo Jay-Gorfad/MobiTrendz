6a0a3cb77106bf087cac1939fdf49eba48a109ba
Jay <jgorfad223@rku.ac.in>
2024-11-28 08:47:57 +0530

    database

--- DB/mobitrendz.sql
+++ DB/mobitrendz.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Oct 22, 2024 at 07:48 AM
+-- Generation Time: Nov 28, 2024 at 03:17 AM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -87,7 +87,7 @@ CREATE TABLE `banner_details_tbl` (
 --
 
 INSERT INTO `banner_details_tbl` (`Banner_Id`, `Banner_Image`, `View_Order`, `Active_Status`) VALUES
-(1, '670a7ae6b300315promaxn1.jpg', 2, 0),
+(1, '6717b2b06dae6_banner15.png', 2, 1),
 (2, '670a7e6db06a9about_img.jpg', 3, 0),
 (4, '67109e83e4596hero.png', 1, 1);
 
@@ -108,11 +108,14 @@ CREATE TABLE `cart_details_tbl` (
 --
 
 INSERT INTO `cart_details_tbl` (`Product_Id`, `Quantity`, `User_Id`) VALUES
+(6, 1, 11),
+(6, 1, 9),
+(6, 1, 10),
+(8, 3, 11),
+(6, 3, 4),
 (8, 1, 4),
-(10, 1, 4),
-(12, 1, 4),
-(6, 1, 4),
-(11, 1, 4);
+(10, 1, 11),
+(14, 1, 4);
 
 -- --------------------------------------------------------
 
@@ -155,7 +158,7 @@ CREATE TABLE `contact_page_details_tbl` (
 --
 
 INSERT INTO `contact_page_details_tbl` (`Contact_Email`, `Contact_Number`) VALUES
-('mobitrendz@gmail.com', '9137284650');
+('new@gmail.com', '9137284651');
 
 -- --------------------------------------------------------
 
@@ -165,22 +168,27 @@ INSERT INTO `contact_page_details_tbl` (`Contact_Email`, `Contact_Number`) VALUE
 
 CREATE TABLE `offer_details_tbl` (
   `Offer_Id` int NOT NULL,
+  `Offer_Code` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `Offer_Description` text,
   `Discount` int DEFAULT NULL,
+  `Max_Discount` float NOT NULL,
   `Minimum_Order` decimal(7,2) DEFAULT NULL,
   `offer_type` int DEFAULT '1',
-  `active_status` int DEFAULT '1'
+  `active_status` int DEFAULT '1',
+  `Start_Date` datetime DEFAULT NULL,
+  `End_Date` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `offer_details_tbl`
 --
 
-INSERT INTO `offer_details_tbl` (`Offer_Id`, `Offer_Description`, `Discount`, `Minimum_Order`, `offer_type`, `active_status`) VALUES
-(1, 'new offers', 10, 500.00, 1, 1),
-(3, 'Offers', 15, 999.00, 2, 1),
-(4, 'OFFERS', 19, 799.00, 3, 1),
-(5, 'new offers 2', 12, 299.00, 1, 1);
+INSERT INTO `offer_details_tbl` (`Offer_Id`, `Offer_Code`, `Offer_Description`, `Discount`, `Max_Discount`, `Minimum_Order`, `offer_type`, `active_status`, `Start_Date`, `End_Date`) VALUES
+(6, '10DISCOUNT', '10% Discount on orders above ₹145', 10, 100, 145.00, 1, 1, '2024-11-21 21:48:06', '2024-11-30 21:48:37'),
+(7, 'FIRSTORDER', 'First purchase discount', 5, 50, NULL, 2, 1, '2024-11-21 21:48:20', '2024-11-30 21:48:37'),
+(8, 'FREESHIPPING', 'Free shipping offer', NULL, 0, 300.00, 3, 1, '2024-11-21 21:48:25', '2024-11-30 21:48:37'),
+(9, '15DISCOUNT', '15% Discount on orders above ₹200', 15, 150, 200.00, 1, 1, '2024-11-14 21:48:28', '2024-11-30 21:48:37'),
+(10, '20DISCOUNT', '20% Discount on orders above ₹300', 20, 60, 300.00, 1, 1, '2024-11-21 21:48:32', '2024-11-30 21:48:37');
 
 -- --------------------------------------------------------
 
@@ -262,11 +270,13 @@ CREATE TABLE `product_details_tbl` (
 --
 
 INSERT INTO `product_details_tbl` (`Product_Id`, `Category_Id`, `Product_Name`, `Description`, `Product_Image`, `Sale_Price`, `Cost_Price`, `Discount`, `stock`, `Display`, `Processor`, `RAM`, `Storage`, `Rear_Camera`, `Front_Camera`, `Battery`, `Operating_System`, `Color`, `is_active`) VALUES
-(6, 2, 'Samsung Galaxy S23 Ultra', 'Experience the power of Galaxy AI with S23 Ultra to effortlessly perfect your photos with Photo Assist, communicate quickly outside your language with Live Translate, 200MP. Wow-worthy resolution, Fast Charging Support, Stereo Speakers, Always On Display, Wireless Charging, Built-In GPS.', '670b6b6e1f2d1samsungs23ultra.jpg', 124999.00, 111000.00, 12, 25, '‎AMOLED', 'Snapdragon 8Gen 2', '12GB', '256GB', '200MP + 12MP + 10MP', '12MP', '‎5000mah', 'Android', 'Cream', 1),
-(8, 1, 'Iphone 11', 'The iPhone 11 boasts a gorgeous all-screen Liquid Retina LCD that is water resistant up to 2 metres for up to 30 minutes.', '670e65a8b4286714Mg+6MoFL._SX679_.jpg', 49999.00, 59999.00, 10, 29, 'XDR Display', 'A13 Bionic', '8GB', '128GB', '48MP + 12MP', '12MP', '4500mah', 'IOS', 'Yellow', 1),
+(6, 2, 'Samsung Galaxy S23 Ultra', 'Experience the power of Galaxy AI with S23 Ultra to effortlessly perfect your photos with Photo Assist, communicate quickly outside your language with Live Translate, 200MP. Wow-worthy resolution, Fast Charging Support, Stereo Speakers, Always On Display, Wireless Charging, Built-In GPS.', '673ddf45e9a86_samsungs23ultra.jpg', 121999.05, 123456.00, 12, 25, '‎AMOLED', 'Snapdragon 8Gen 2', '12GB', '256GB', '200MP + 12MP + 10MP', '12MP', '‎5000mah', 'Android', 'Cream', 1),
+(8, 1, 'Iphone 16', 'The iPhone 11 boasts a gorgeous all-screen Liquid Retina LCD that is water resistant up to 2 metres for up to 30 minutes.', '670e65a8b4286714Mg+6MoFL._SX679_.jpg', 10000.00, 10000.00, 10, 29, 'XDR Display', 'A13 Bionic', '8GB', '128GB', '48MP + 12MP', '20MP', '4500mah', 'IOS', 'Yellow', 1),
 (10, 3, 'Realme NARZO 70x', '7.69 mm Ultra-slim ,188g Light Body, The ultra-thin and lightweight body, combined with a width of 7.97cm, allows for comfortable single-handed grip even during extended periods of use; IP54 Dust and Water Resistance;', '670e68eeadc81realmenarzo70x.jpg', 13999.00, 16999.00, 10, 64, '120Hz Ultra Smooth Display', 'MediaTek Dimensity 6100+ processor', '8GB', '128GB', '48MP + 12MP + 8MP', '12MP', '‎5000mah', 'Android', 'Blue', 1),
 (11, 6, 'OPPO A3X 5G', '16.94 cm (6.67\"Inch) HD+ LCD 120Hz Ultra Bright Display to greatly improve the smoothness of screen touches ,with Screen-to-body ratio of 89.9% for better viewing experience.', '670e6afb553d5oppoa3x.jpg', 16999.00, 17999.00, 18, 48, '120Hz Refresh Rate Display', ' MediaTek Dimensity 6300', '8GB', '128GB', '48MP + 12MP', '12MP', '4500mah', 'Android', 'Black', 1),
-(12, 7, 'Vivo Y28s', '16.6624 cm (6.56\" inch) LCD Capacitive multi-touch display 90Hz refresh rate, 269 ppi & 840 nits, Side-mounted capacitive fingerprint sensor.', '670e6ce2dd31fvivoy28s.jpg', 14599.00, 17999.00, 13, 76, '6.56\" inch LCD Display', 'Dimensity 6300 5G processor', '8GB', '128GB', '48MP + 12MP', '12MP', '4500mah', 'Android', 'Red', 1);
+(12, 7, 'Vivo Y28s', '16.6624 cm (6.56\" inch) LCD Capacitive multi-touch display 90Hz refresh rate, 269 ppi & 840 nits, Side-mounted capacitive fingerprint sensor.', '670e6ce2dd31fvivoy28s.jpg', 14599.00, 17999.00, 13, 76, '6.56\" inch LCD Display', 'Dimensity 6300 5G processor', '8GB', '128GB', '48MP + 12MP', '12MP', '4500mah', 'Android', 'Red', 1),
+(13, 8, 'asasf', 'sdafafaas', '671869c0b4d28samsungs23ultra.jpg', 10.00, 10.00, 10, 10, 'XDR Display', 'asf', '12Gb', '128GB', '48MP + 12MP', '12MP', '4500mah', 'Android', 'Cream', 0),
+(14, 1, 'Iphone 15 Pro', 'iphone', '67409349bfcac_71yzJoE7WlL._SX679_.jpg', 150000.00, 150000.00, 10, 2, '6.7” AMOLED Display', ' MediaTek Dimensity 6300', '12Gb', '512GB', '48MP + 12MP + 8MP', '12MP', '‎5000mah', 'IOS', 'Green', 1);
 
 -- --------------------------------------------------------
 
@@ -289,7 +299,9 @@ CREATE TABLE `responses_tbl` (
 
 INSERT INTO `responses_tbl` (`Response_Id`, `First_Name`, `Last_Name`, `Email`, `Phone`, `Message`) VALUES
 (7, 'Yagnik', 'Desai', 'ydesai000@gmail.com', '6353915155', 'Details About Upcoming Mobile Phones'),
-(8, 'Jay', 'Gorfad', 'jaygorfad00@gmail.com', '7600242424', 'About Mobile Phone');
+(8, 'Jay', 'Gorfad', 'jaygorfad00@gmail.com', '7600242424', 'About Mobile Phone'),
+(10, 'jayyy', 'Gorfad', 'ydesai000@rku.ac.in', '6353915155', 'xyz'),
+(11, 'Bhakti', 'Bhut', 'patelbhakti636@gmail.com', '7896521430', 'New Message');
 
 -- --------------------------------------------------------
 
@@ -312,17 +324,8 @@ CREATE TABLE `review_details_tbl` (
 --
 
 INSERT INTO `review_details_tbl` (`Review_Id`, `Reply_To`, `Product_Id`, `User_Id`, `Rating`, `Review`, `Review_Date`) VALUES
-(4, NULL, 3, 5, 5, 'Good Device', '2024-10-13 19:08:48'),
-(7, 4, NULL, 1, NULL, 'Good', '2024-10-13 19:12:00'),
-(8, NULL, 5, 4, 4, 'Nice Device', '2024-10-13 19:12:23'),
-(9, 8, NULL, 1, NULL, 'good', '2024-10-13 19:16:01'),
-(10, NULL, 12, 4, 4, 'Good Product', '2024-10-15 18:56:51'),
-(11, 10, NULL, 1, NULL, 'Good', '2024-10-15 18:57:08'),
-(12, NULL, 6, 4, 5, 'Good Product', '2024-10-17 11:30:16'),
-(13, NULL, 8, 4, 1, 'Good Product', '2024-10-17 11:30:50'),
-(14, NULL, 10, 4, 3, 'Good Product', '2024-10-17 11:31:13'),
-(15, NULL, 11, 4, 2, 'Good Mobile', '2024-10-17 11:31:31'),
-(16, NULL, 6, 8, 5, 'Awesome Mobile', '2024-10-18 23:36:44');
+(16, NULL, 6, 8, 5, 'Awesome Mobile', '2024-10-18 23:36:44'),
+(17, 12, NULL, 8, NULL, 'aabc', '2024-10-23 09:40:56');
 
 -- --------------------------------------------------------
 
@@ -337,18 +340,20 @@ CREATE TABLE `user_details_tbl` (
   `Password` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
   `Email` varchar(320) COLLATE utf8mb4_general_ci NOT NULL,
   `Mobile_No` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
-  `Active_Status` tinyint NOT NULL
+  `Active_Status` tinyint NOT NULL,
+  `Profile_Picture` varchar(255) COLLATE utf8mb4_general_ci DEFAULT 'default-img.png'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `user_details_tbl`
 --
 
-INSERT INTO `user_details_tbl` (`User_Id`, `User_Role_Id`, `Name`, `Password`, `Email`, `Mobile_No`, `Active_Status`) VALUES
-(4, 0, 'Yagnik Desai', '12345678', 'ydesai000@rku.ac.in', '987456320', 1),
-(6, 0, 'Bhautik Kacha', '12345678', 'bhautik@gmail.com', '9475681499', 0),
-(7, 0, 'Prince Bhatt', '12345678', 'p@gmail.com', '9999254569', -1),
-(8, 1, 'Jay Gorfad', '12345678', 'jaygorfad00@gmail.com', '7600242424', 1);
+INSERT INTO `user_details_tbl` (`User_Id`, `User_Role_Id`, `Name`, `Password`, `Email`, `Mobile_No`, `Active_Status`, `Profile_Picture`) VALUES
+(4, 0, 'Yagnik Desai', '23122312', 'ydesai000@rku.ac.in', '9874563200', 1, '67189fe87a6a4_download.png'),
+(6, 0, 'Bhautik Kacha', '12345678', 'bhautik@gmail.com', '9475681499', 0, 'default-img.png'),
+(7, 0, 'Prince Bhatt', '12345678', 'p@gmail.com', '9999254569', 1, 'default-img.png'),
+(8, 1, 'Jay Gorfad', '12345678', 'jaygorfad00@gmail.com', '7600242424', 1, 'default-img.png'),
+(11, 0, 'Jay Barasiya', '12345678', 'jbarasiya302@rku.ac.in', '9876543210', 1, '67189310d9e97_shirt photo.jpg');
 
 -- --------------------------------------------------------
 
@@ -445,7 +450,7 @@ ALTER TABLE `category_details_tbl`
 -- AUTO_INCREMENT for table `offer_details_tbl`
 --
 ALTER TABLE `offer_details_tbl`
-  MODIFY `Offer_Id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `Offer_Id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `order_header_tbl`
@@ -457,25 +462,25 @@ ALTER TABLE `order_header_tbl`
 -- AUTO_INCREMENT for table `product_details_tbl`
 --
 ALTER TABLE `product_details_tbl`
-  MODIFY `Product_Id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `Product_Id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `responses_tbl`
 --
 ALTER TABLE `responses_tbl`
-  MODIFY `Response_Id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `Response_Id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `review_details_tbl`
 --
 ALTER TABLE `review_details_tbl`
-  MODIFY `Review_Id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `Review_Id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `user_details_tbl`
 --
 ALTER TABLE `user_details_tbl`
-  MODIFY `User_Id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `User_Id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
